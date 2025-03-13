5f06666f64840dfc2c163336a01653a1e572c28f
liuyu <>
2025-03-13 19:46:24 +0800

    fix: use env value

--- util/nginx/command.go
+++ util/nginx/command.go
@@ -9,7 +9,7 @@ import (
 	"k8s.io/klog/v2"
 )
 
-const (
+var (
 	DefNgxBinary  = "/sbin/nginx"
 	DefNgxCfgPath = "/etc/nginx/nginx.conf"
 )
@@ -30,11 +30,13 @@ func NewCommand() *Command {
 	binary := os.Getenv("NGINX_BINARY")
 	if binary != "" {
 		command.binary = binary
+		DefNgxBinary = binary
 	}
 
 	ngxCfgPath := os.Getenv("NGINX_CONF_PATH")
 	if ngxCfgPath != "" {
 		command.confPath = ngxCfgPath
+		DefNgxCfgPath = ngxCfgPath
 	}
 	return &command
 }
